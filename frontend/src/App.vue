2e028fe237f2500b48dda942b4fca5eb4ed1def2
Chairil ashar <asharchairil4@gmail.com>
2022-10-25 20:59:52 +0700

    hasdone

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,91 +1,65 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
+  <div id="app">
+    <select-username
+      v-if="!usernameAlreadySelected"
+      @input="onUsernameSelection"
     />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    <chat v-else @logOut="logOut" />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<script>
+import SelectUsername from "./components/SelectUsername.vue";
+import Chat from "./components/Chat.vue";
+import socket from "./ socket";
+export default {
+  name: "App",
+  components: {
+    Chat,
+    SelectUsername,
+  },
+  data() {
+    return {
+      usernameAlreadySelected: false,
+    };
+  },
+  methods: {
+    onUsernameSelection(username) {
+      console.log("username", username);
+      this.usernameAlreadySelected = true;
+      socket.auth = { username };
+      socket.connect();
+    },
+    logOut() {
+      //localStorage.removeItem("sessionID");
+      localStorage.setItem("sessionID", "");
+      this.usernameAlreadySelected = false;
+    },
+  },
+  created() {
+    const sessionID = localStorage.getItem("sessionID");
+    if (sessionID) {
+      this.usernameAlreadySelected = true;
+      socket.auth = { sessionID };
+      socket.connect();
+    }
+    socket.on("session", ({ sessionID, userID }) => {
+      // attach the session ID to the next reconnection attempts
+      socket.auth = { sessionID };
+      // store it in the localStorage
+      localStorage.setItem("sessionID", sessionID);
+      // save the ID of the user
+      socket.userID = userID;
+    });
+    socket.on("connect_error", (err) => {
+      if (err.message === "invalid username") {
+        this.usernameAlreadySelected = false;
+        console.log("ini error Login");
+      }
+    });
+  },
+  unmounted() {
+    socket.off("connect_error");
+  },
+};
+</script>
